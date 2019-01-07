881b39a24a8e449530f0bdb968ac338a3e16792f
John Reeves <jreeves@k12.com>
2019-01-07 13:05:05 -0800

    post mockage

--- src/app/forecast/forecast.module.ts
+++ src/app/forecast/forecast.module.ts
@@ -1,8 +1,8 @@
 import { IonicModule } from '@ionic/angular';
 import { RouterModule } from '@angular/router';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { ForecastPage } from './forecast.page';
 
 @NgModule({
