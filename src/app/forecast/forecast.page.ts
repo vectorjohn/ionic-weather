d4f4b935ba31ebb7eb1269baad8ce65baf66d223
John Reeves <jreeves@k12.com>
2019-01-09 06:17:46 -0800

    firebase config

--- src/app/forecast/forecast.page.ts
+++ src/app/forecast/forecast.page.ts
@@ -14,29 +14,6 @@ import {NetworkService} from '../services/network/network.service';
 })
 export class ForecastPage {
   forecast: Forecast;
-  /*= [
-    [
-      {
-        temperature: 300,
-        condition: 200,
-        date: new Date(2018, 8, 19)
-      }
-    ],
-    [
-      {
-        temperature: 265,
-        condition: 601,
-        date: new Date(2018, 8, 20)
-      }
-    ],
-    [
-      {
-        temperature: 293,
-        condition: 800,
-        date: new Date(2018, 8, 21)
-      }
-    ]
-  ];*/
 
   constructor(public iconMap: IconMapService,
               private weather: WeatherService,
